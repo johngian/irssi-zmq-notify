3b81c9e4065754520f335932770b49b3da222395
John Giannelos <johngiannelos@gmail.com>
2013-05-05 18:02:47 +0300

    Initial implementation of desktop notifications

--- irssiMQ/irssiNotifyServer.py
+++ irssiMQ/irssiNotifyServer.py
@@ -1,4 +1,5 @@
 import argparse
+import pynotify
 import zmq
 
 
@@ -15,10 +16,14 @@ def main():
     server_id = 'local-irssi'
     socket.connect("tcp://%s:%d" % (args.host, args.port))
 
+    pynotify.init('IRSSI notifications')
+
     while True:
         message = socket.recv()
+        notification = pynotify.Notification("IRSSI", message)
         print "Received request:", message
         socket.send("ACK from server %s" % server_id)
+        notification.show()
         
 if __name__ == "__main__":
     main()
